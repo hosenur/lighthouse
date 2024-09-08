08654b3ad88b54ad9810b359526ee8eb2825144e
hosenur <hosenur.dev@gmail.com>
2024-09-08 15:31:01 +0530

    test

--- app/controllers/apps_controller.ts
+++ app/controllers/apps_controller.ts
@@ -1,15 +1,11 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import pm2 from 'pm2'
-import dockerode from 'dockerode'
 export default class AppsController {
     async index({ inertia }: HttpContext) {
-        const docker = new dockerode()
         const PM2Apps = await pm2.list((e) => {
             console.log(e)
         })
-        const DockerApps = docker.listContainers()
         console.log(PM2Apps)
-        console.log(DockerApps)
         return inertia.render('home')
 
     }
